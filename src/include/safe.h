c948274a64e9d4e4cf97eb679730350ee02470ab
Chen Liang <chen.liang.cn@outlook.com>
2025-04-13 11:15:51 +0800

    attempting to create Result and Option enum in C

--- src/include/safe.h
+++ src/include/safe.h
@@ -1,51 +1,15 @@
-enum Result {
-    Ok,
-    Error,
+struct Result {
+    enum ResultEnum {
+        Ok,
+        Error,
+    };
+
+    struct Option {
+        enum OptionEnum {
+            Some,
+            None,
+        };
+
+        void *data_ptr;
+    };
 };
-
-enum Option {
-    Some,
-    None,
-};
-
-typedef struct {
-    enum Result execution_result;
-    short data;
-} ResultInt16;
-
-typedef struct {
-    enum Result execution_result;
-    int data;
-} ResultInt32;
-
-typedef struct {
-    enum Result execution_result;
-    long data;
-} ResultInt64;
-
-typedef struct {
-    enum Result execution_result;
-    long long data;
-} ResultInt128;
-
-typedef struct {
-    enum Result execution_result;
-    float data;
-} ResultFloat32;
-
-typedef struct {
-    enum Result execution_result;
-    double data;
-} ResultFloat64;
-
-
-typedef struct {
-    enum Result execution_result;
-    char data;
-} ResultChar;
-
-typedef struct {
-    enum Result execution_result;
-    char data;
-    unsigned int length;
-} ResultString;
