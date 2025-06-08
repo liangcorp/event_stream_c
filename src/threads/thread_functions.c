597e215b2f9487b969d99c5854b3b01435e52319
Chen Liang <chen.liang.cn@outlook.com>
2025-06-09 00:07:21 +0800

    add a default html 200 return page

--- src/threads/thread_functions.c
+++ src/threads/thread_functions.c
@@ -8,8 +8,31 @@
 
 #include "thread_functions.h"
 
-#define MAX_MESSAGE_SIZE 250
+#define MAX_MESSAGE_SIZE 500
 #define MAX_THREAD_NUMBER 1
+
+const char return_http_200_default[] = "HTTP/1.1 200 OK \n\
+Accept-Ranges: bytes \n\
+Age: 294510 \n\
+Cache-Control: max-age=604800 \n\
+Content-Type: text/html; charset=UTF-8 \n\
+Date: Fri, 21 Jun 2025 14:18:33 GMT \n\
+Etag: \"3147526947\" \n\
+Expires: Fri, 28 Jun 2025 14:18:33 GMT \n\
+Last-Modified: Thu, 17 Oct 2019 07:18:26 GMT \n\
+Server: ECAcc (nyd/D10E) \n\
+X-Cache: HIT \n\
+\n\
+<!doctype html> \n\
+<!-- HTML content follows here -->\n\
+<html>\n\
+    <head>\n\
+    <title>test page</title>\n\
+    </head>\n\
+    <body>\n\
+        Hello\n\
+    </body>\n\
+</html>";
 
 void *hello_fun(void *thread_worker_var)
 {
@@ -36,7 +59,7 @@ void *hello_fun(void *thread_worker_var)
     snprintf(outgoing_message, MAX_MESSAGE_SIZE, "Hello %d", thread_variable.socket);
 
     /* send some data */
-    if (send(thread_variable.socket, outgoing_message, MAX_MESSAGE_SIZE, 0) < 0)
+    if (send(thread_variable.socket, return_http_200_default, MAX_MESSAGE_SIZE, 0) < 0)
     {
         int errsv = errno;
         fprintf(stderr, "SOCKET send ERROR <%s:%d>: %s", __FILE__, __LINE__,
